8cb9c2a6d7d9ede96a42dffa838e6790cb90776c
AdnanAlrashed <adnanalrashed77@gmail.com>
2025-09-13 22:22:19 +0300

    ee

--- videodownloader/settings.py
+++ videodownloader/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-5&k^3&6ghj5u=_j-flds97$10agte3+)8xq1$$=kki1d*dm3u5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["videodownloader-1-47ot.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
